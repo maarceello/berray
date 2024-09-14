ce41704712eeb1a70c6143374f756e6d02168d0d
marfsama <7756076+marfsama@users.noreply.github.com>
2024-09-14 14:30:43 +0200

    add java parser to generate documentation. prototype.

--- src/main/java/com/berraydoc/BerrayDoc.java
+++ src/main/java/com/berraydoc/BerrayDoc.java
@@ -1,5 +1,7 @@
 package com.berraydoc;
 
+import com.berray.components.core.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
@@ -7,7 +9,6 @@ import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.comments.Comment;
-import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.ast.expr.Name;
 import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
@@ -24,15 +25,23 @@ public class BerrayDoc {
     SourceRoot sourceRoot = new SourceRoot(FileSystems.getDefault().getPath("./src/main/java"));
     List<ParseResult<CompilationUnit>> parseResults = sourceRoot.tryToParse();
 
+    List<ClassDocumentation> classes = new ArrayList<>();
+
     System.out.println("----------------");
     for (ParseResult<CompilationUnit> parseResult : parseResults) {
       FindJavaDocVisitor visitor = new FindJavaDocVisitor();
       visitor.visit(parseResult.getResult().get(), "");
-      visitor.finishedClasses.forEach(c -> {
-        System.out.println(visitor.packageName + "." + c.getName());
-        System.out.println("  "+c.extendedClasses);
-      });
+      visitor.finishedClasses
+          .stream().filter(c -> c.extendedClasses.contains(Component.class.getName()))
+          .forEach(c -> {
+            System.out.println(visitor.packageName + "." + c.getName());
+            System.out.println("  extends " + c.extendedClasses);
+            System.out.println("  " + c.classJavaDoc);
+            System.out.println(c.methods);
+            classes.add(c);
+          });
     }
+    System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(classes));
   }
 
   private static class FindJavaDocVisitor extends BerrayVisitorAdapter<Boolean, String> {
@@ -57,7 +66,12 @@ public class BerrayDoc {
     @Override
     public Boolean visit(ClassOrInterfaceDeclaration n, String arg) {
       // create new class documentation holder and place them on the stack
-      classStack.add(new ClassDocumentation(getClassName(n.getName().getIdentifier())));
+      String className = getClassName(n.getName().getIdentifier());
+      ClassDocumentation documentation = new ClassDocumentation(className);
+      classStack.push(documentation);
+      n.getComment().ifPresent(
+          comment -> documentation.setClassJavaDoc(comment.getContent())
+      );
       try {
         return super.visit(n, arg);
       } finally {
@@ -74,7 +88,7 @@ public class BerrayDoc {
       return classStack.stream()
           .map(ClassDocumentation::getName)
           .collect(Collectors.joining("."))
-          +"."+thisClassName;
+          + "." + thisClassName;
     }
 
     @Override
@@ -87,6 +101,7 @@ public class BerrayDoc {
     public Boolean visit(MethodDeclaration n, String arg) {
       if (n.getComment().isPresent()) {
         Comment comment = n.getComment().get();
+        classStack.peek().addMethod(new MethodDocumentation(n.getName().asString(), comment.asString()));
       }
       return super.visit(n, arg);
     }
@@ -94,9 +109,9 @@ public class BerrayDoc {
     private String getFullQualifiedName(SimpleName name) {
       String lastPart = name.getIdentifier();
       if (imports.containsKey(lastPart)) {
-        return imports.get(lastPart)+"."+lastPart;
+        return imports.get(lastPart) + "." + lastPart;
       }
-      return packageName+"."+lastPart;
+      return packageName + "." + lastPart;
     }
 
   }
@@ -104,7 +119,9 @@ public class BerrayDoc {
 
   private static class ClassDocumentation {
     private String name;
+    private String classJavaDoc;
     private List<String> extendedClasses = new ArrayList<>();
+    private List<MethodDocumentation> methods = new ArrayList<>();
 
     public ClassDocumentation(String name) {
       this.name = name;
@@ -114,14 +131,51 @@ public class BerrayDoc {
       return name;
     }
 
+    public String getClassJavaDoc() {
+      return classJavaDoc;
+    }
+
+    public List<MethodDocumentation> getMethods() {
+      return methods;
+    }
+
     public void addExtends(String name) {
       extendedClasses.add(name);
+    }
+
+    public void addMethod(MethodDocumentation method) {
+      this.methods.add(method);
+    }
+
+
+    public void setClassJavaDoc(String classJavaDoc) {
+      this.classJavaDoc = classJavaDoc;
     }
   }
 
   private static class MethodDocumentation {
     private String name;
     private String documentation;
-    private List<String> params = new ArrayList<>();
+
+    public MethodDocumentation(String name, String documentation) {
+      this.name = name;
+      this.documentation = documentation;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public String getDocumentation() {
+      return documentation;
+    }
+
+    @Override
+    public String toString() {
+      return "MethodDocumentation{" +
+          "name='" + name + '\'' +
+          ", documentation='" + documentation + '\'' +
+          '}';
+    }
   }
 }

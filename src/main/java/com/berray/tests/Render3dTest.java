e8f00fbd5f3d56713e0f00e9da67da68be305694
marfsama <7756076+marfsama@users.noreply.github.com>
2024-10-21 12:09:45 +0200

    render indirect by collection render methods instead of game objects.

--- src/main/java/com/berray/tests/Render3dTest.java
+++ src/main/java/com/berray/tests/Render3dTest.java
@@ -3,6 +3,7 @@ package com.berray.tests;
 import com.berray.BerrayApplication;
 import com.berray.GameObject;
 import com.berray.components.CoreComponentShortcuts;
+import com.berray.components.core.Component;
 import com.berray.components.core.Rotate3dComponent;
 import com.berray.math.Color;
 import com.berray.math.Vec3;
@@ -10,6 +11,8 @@ import com.berray.objects.CameraComponent;
 import com.berray.objects.Root3D;
 import com.raylib.Jaylib;
 import com.raylib.Raylib;
+
+import static com.raylib.Raylib.*;
 
 public class Render3dTest extends BerrayApplication implements CoreComponentShortcuts {
 
@@ -20,12 +23,16 @@ public class Render3dTest extends BerrayApplication implements CoreComponentShor
   public void game() {
 
     targetFps = -1;
-    add(
+    Root3D root3d = add(
         new Root3D(),
         pos(new Vec3(0.0f, 0.0f, 0.0f)),
         Rotate3dComponent.rotate(),
         CameraComponent.camera(camera),
         "root3d"
+    );
+
+    root3d.add(
+        new CubeComponent()
     );
 
     onUpdate("root3d", event -> {
@@ -60,5 +67,27 @@ public class Render3dTest extends BerrayApplication implements CoreComponentShor
 
   public static void main(String[] args) {
     new Render3dTest().runGame();
+  }
+
+  private static class CubeComponent extends Component {
+    public CubeComponent() {
+      super("cube");
+    }
+
+    @Override
+    public void add(GameObject gameObject) {
+      super.add(gameObject);
+      registerGetter("render", () -> true);
+    }
+
+    @Override
+    public void draw() {
+      rlPushMatrix();
+      {
+          rlMultMatrixf(gameObject.getWorldTransform().toFloatTransposed());
+          Raylib.DrawCubeWires(new Vector3(), 1.0f,1.0f,1.0f, Jaylib.WHITE);
+      }
+      rlPopMatrix();
+    }
   }
 }
